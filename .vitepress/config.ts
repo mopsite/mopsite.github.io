b8f6a687210c1af8fce8a75c843b522265ee44b4
paul <37662788@qq.com>
2024-02-05 23:09:33 +0800

    blog

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -36,8 +36,11 @@ export default defineConfig({
     sidebarMenuLabel: '菜单',
     returnToTopLabel: '返回顶部',
     search: {
-      provider: 'local',
+      provider: 'algolia',
       options: {
+        indexName: 'mopio',
+        appId: '3SO0DJQKCD',
+        apiKey: 'a5958b3863ace676771d60ba7d1ec174',
         locales: {
           root: {
             translations: {
@@ -46,13 +49,35 @@ export default defineConfig({
                 buttonAriaLabel: '搜索文档'
               },
               modal: {
-                noResultsText: '无法找到相关结果',
-                resetButtonTitle: '清除查询条件',
-                displayDetails: '显示详细列表',
+                searchBox: {
+                  resetButtonTitle: '清除查询条件',
+                  resetButtonAriaLabel: '清除查询条件',
+                  cancelButtonText: '取消',
+                  cancelButtonAriaLabel: '取消'
+                },
+                startScreen: {
+                  recentSearchesTitle: '搜索历史',
+                  noRecentSearchesText: '没有搜索历史',
+                  saveRecentSearchButtonTitle: '保存至搜索历史',
+                  removeRecentSearchButtonTitle: '从搜索历史中移除',
+                  favoriteSearchesTitle: '收藏',
+                  removeFavoriteSearchButtonTitle: '从收藏中移除'
+                },
+                errorScreen: {
+                  titleText: '无法获取结果',
+                  helpText: '你可能需要检查你的网络连接'
+                },
                 footer: {
                   selectText: '选择',
                   navigateText: '切换',
-                  closeText: '关闭'
+                  closeText: '关闭',
+                  searchByText: '搜索提供者'
+                },
+                noResultsScreen: {
+                  noResultsText: '无法找到相关结果',
+                  suggestedQueryText: '你可以尝试查询',
+                  reportMissingResultsText: '你认为该查询应该有结果？',
+                  reportMissingResultsLinkText: '点击反馈'
                 }
               }
             }
